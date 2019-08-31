273eac272b32b30a3cc8a3d0a33b909a06cfa06d
Ares Regalado <44949355+AresRegalado@users.noreply.github.com>
2019-08-31 17:54:10 -0500

    Add files via upload

---
+++ Tarea8.js
@@ -0,0 +1,11 @@
+
+const  abs  =  function ( num1 ) {
+    if (num1 < 0)
+        return num1 * -1
+    else 
+        return num1 
+  }
+console.log ( abs ( -2  )) // 6
+console.log ( abs ( 8  )) // 3
+console.log ( abs ( -7  )) // 18
+console.log ( abs ( 9 )) // 3
